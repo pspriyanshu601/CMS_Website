628fd4a638b1d920af19ac0340f5b84b540c22c4
pspriyanshu601 <priyanshusingh6014@gmail.com>
2024-05-16 15:37:26 +0530

    signup and signin page

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,42 +4,54 @@ import User from "../models/user.js";
 const userRouter = express.Router();
 
 userRouter.post("/signup", async (req, res) => {
-  const { fullName, email, password } = req.body;
-  if (!fullName || !email || !password) {
-    return res.status(400).json({
-      success: false,
-      error: "Please provide fullName, email, and password",
-    });
-  }
-  const alreadyPresent = User.findOne({ email: email });
-  console.log(alreadyPresent);
-  if (alreadyPresent !== null) {
-    return res.status(400).json({
-      message: "this user already exist",
-      success: false,
-      path: "login",
-    });
-  }
+  try {
+    const { fullName, email, password } = req.body;
+    if (!fullName || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        error: "Please provide fullName, email, and password",
+      });
+    }
+    const alreadyPresent = User.findOne({ email:email });
+    console.log(alreadyPresent.fullName);
+    if (alreadyPresent != null) {
+      return res.status(400).json({
+        message: "this user already exist",
+        success: false,
+        path: "signin",
+      });
+    }
 
-  await User.create({
-    fullName,
-    email,
-    password,
-  });
-  return res.status(200).json({
-    success: true,
-    message: "User Created successfully",
-  });
+    await User.create({
+      fullName,
+      email,
+      password,
+    });
+    return res.status(200).json({
+      success: true,
+      message: "User Created successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({
+      message: "Internal server error",
+      success: false,
+    });
+  }
 });
 
 userRouter.post("/signin", async (req, res) => {
   try {
     const { email, password } = req.body;
     const token = await User.matchPassword(email, password);
+    const user = User.findOne({ email: email });
 
     return res.cookie("token", token).status(200).json({
       success: true,
       message: "Logged in successfully",
+      username: user.fullName,
+      path: "home",
+      token,
     });
   } catch (error) {
     console.log(error);
